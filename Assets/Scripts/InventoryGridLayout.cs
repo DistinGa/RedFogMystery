95547d6bb88a20f9c6ee9182c8aa8d51ab366549
Dmitriy <dima.maliy@gmail.com>
2016-06-11 08:24:17 +0300

    Меню, по просььбу Димы

---
+++ Assets/Scripts/InventoryGridLayout.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InventoryGridLayout : MonoBehaviour
+{
+    public int row;
+    void Start()
+    {
+        RectTransform parent = gameObject.GetComponent<RectTransform>();
+        GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup>();
+
+        grid.cellSize = new Vector2(parent.rect.width / 2, parent.rect.height / row);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+
+    private Animator _animator;
+    private CanvasGroup _canvasGroup;
+    private CharacterMoving _controller;
+
+    public bool IsOpen
+    {
+        get { return _animator.GetBool("IsOpen"); }
+        set { _animator.SetBool("IsOpen", value); }
+    }
+
+    public void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _controller = FindObjectOfType<CharacterMoving>();
+
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+    }
+
+    public void Update()
+    {
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+            _canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+        else
+            _canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+            {
+                BlockMoving(true);
+                IsOpen = true;
+            }
+            else
+            {
+                BlockMoving(false);
+                IsOpen = false;
+            }
+        }
+    }
+    private void BlockMoving(bool block)
+    {
+        _controller.KeyboardControl = !block;
+    }
+}
